7e7b7fff1ba17abe45f0770b708b8ca209b1b7d4
Erencan Ekinci <kyse1234@hotmail.com>
2025-01-24 04:08:36 +0300

    first commit

---
+++ graph/chains/hallucination_grader.py
@@ -0,0 +1,28 @@
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class GradeHallucination(BaseModel):
+    """Binary score for hallucination present in generated answer"""
+
+    binary_score: str = Field(
+        description="Answer is grounded in the facts, 'yes' or 'no'"
+    )
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0)
+structured_llm_grader = llm.with_structured_output(GradeHallucination)
+
+system_prompt = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts.
+Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts."""
+
+hallucination_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",system_prompt),
+        ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}")
+    ]
+)
+
+hallucination_grader = hallucination_prompt | structured_llm_grader
