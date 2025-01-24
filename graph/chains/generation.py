9a5926507ada8890ba525ddf1a638364a0ef4017
Erencan Ekinci <kyse1234@hotmail.com>
2025-01-24 04:24:00 +0300

    first commit

---
+++ graph/chains/generation.py
@@ -0,0 +1,17 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0.5)
+prompt = ChatPromptTemplate.from_template(""" 
+You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+Question: {question} 
+Context: {context} 
+Answer:
+""") # "rlm/rag-prompt" from langchain hub
+
+generation_chain = prompt | llm | StrOutputParser()
